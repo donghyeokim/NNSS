ad879bd84fc7980e45512c61234f773c266c3fad
zting94 <kyk0128tt@gmail.com>
2021-06-13 22:58:02 +0900

    initialize NNSS

---
+++ src/main/java/com/nnss/NNSS/NnssApplication.java
@@ -0,0 +1,13 @@
+package com.nnss.NNSS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NnssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nnss/NNSS/NnssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nnss.NNSS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
